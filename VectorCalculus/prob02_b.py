1e8aaf556e2fc3ce47768026b1c3b7cd283101b5
Sheshanth Vijay <157735999+SVijayakumar04@users.noreply.github.com>
2026-02-13 12:39:31 -0500

    Update Du function to compute a 2D array

--- VectorCalculus/prob02_b.py
+++ VectorCalculus/prob02_b.py
@@ -3,7 +3,11 @@ import numpy as np
 def Du(x_1,x_2):
     
     ### <--- START OF YOUR CODE
-    Du= np.array(0)
+   
+    Du = np.array([
+        [x_2,   x_1],
+        [2*x_1, -2*x_2]
+    ])
 
     ### END OF YOUR CODE --->
 
