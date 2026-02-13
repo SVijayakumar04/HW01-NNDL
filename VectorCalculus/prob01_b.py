8c45be3eda6d7e6ddb224abacf614e216c4a7347
Sheshanth Vijay <157735999+SVijayakumar04@users.noreply.github.com>
2026-02-13 12:35:36 -0500

    Implement partial derivatives in Df function

--- VectorCalculus/prob01_b.py
+++ VectorCalculus/prob01_b.py
@@ -4,10 +4,11 @@ def Df(u,v,p,t):
     
     ### <--- START OF YOUR CODE
 
-    f_u = 0
-    f_v = 0
-    f_p = 0
-    f_t = 0
+    f_u = 16*u*(t**3)*p + 4*u*t
+    f_v = -(p**2)*(t**(-5)) / (2*np.sqrt(v)) - 1
+    f_p = 8*(u**2)*(t**3) - 2*np.sqrt(v)*p*(t**(-5)) + 12*(p**3)
+    f_t = 24*(u**2)*(t**2)*p + 5*np.sqrt(v)*(p**2)*(t**(-6)) + 2*(u**2)
+
 
     ### END OF YOUR CODE --->
 
