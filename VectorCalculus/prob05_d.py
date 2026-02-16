dd8a53c65fdf61ca714b020eab1d84552b56fb39
Sheshanth Vijay <157735999+SVijayakumar04@users.noreply.github.com>
2026-02-16 18:24:12 -0500

    Update dfogoh function to apply chain rule
    Implement chain rule in dfogoh function to compute derivatives.

--- VectorCalculus/prob05_d.py
+++ VectorCalculus/prob05_d.py
@@ -15,7 +15,20 @@ def h(x_1,x_2,x_3):
 def dfogoh(x_1,x_2,x_3):
     
     ### <--- START OF YOUR CODE
-    dfogoh = np.array(0)
+    
+    # h(x) is (2,1)
+    hx = h(x_1, x_2, x_3)
+    h1 = hx[0, 0]
+    h2 = hx[1, 0]
+
+    # g(h(x)) is (2,1)
+    gh = g(h1, h2)
+    z1 = gh[0, 0]
+    z2 = gh[1, 0]
+
+    # chain rule: (1,2) @ (2,2) @ (2,3) -> (1,3)
+    dfogoh = Df(z1, z2) @ Dg(h1, h2) @ Dh(x_1, x_2, x_3)
+
     ### END OF YOUR CODE --->
 
     return dfogoh
