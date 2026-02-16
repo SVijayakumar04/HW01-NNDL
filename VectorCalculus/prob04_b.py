2a9a0361eebeaf990de5c764a7dd110d47e5d573
Sheshanth Vijay <157735999+SVijayakumar04@users.noreply.github.com>
2026-02-16 18:16:11 -0500

    Implement second derivative calculation in d2Bdt2

--- VectorCalculus/prob04_b.py
+++ VectorCalculus/prob04_b.py
@@ -3,7 +3,13 @@ import numpy as np
 def d2Bdt2(t):
     
     ### <--- START OF YOUR CODE
-    d2Bdt2 = 0
+
+    D = -t**4 - t**3 + 6*t**2 - 4*t - 3
+    Dp = -4*t**3 - 3*t**2 + 12*t - 4
+    Dpp = -12*t**2 - 6*t + 12
+
+    d2Bdt2 = (Dpp*D - Dp**2) / (D**2)
+
     ### END OF YOUR CODE --->
 
     return d2Bdt2
